50b770020a2b1b76d2a107a8f158c54875f2d0c3
SYED MUHTASIM ALAM <168188850+muhtasim-apon@users.noreply.github.com>
2025-11-23 20:39:46 +0600

    well this is final

--- apon.s
+++ apon.s
@@ -1,21 +1,7 @@
         AREA apon, CODE, READONLY
-<<<<<<< HEAD
         EXPORT apon_main
 
 apon_main
-        MOV     R1, #5
-        MOV     R2, #1
-        LSL     R3, R1, R2       ; R3 = R1 << R2 (5 << 1 = 10)
-
-        LDR     R0, =0x20000000
-        STR     R3, [R0]
-
-        BX      LR
-=======
-        ENTRY
-        EXPORT main
-
-main
         ; Load a number into R1 (example: 0x5)
         MOV    R1, #5
 
@@ -29,8 +15,6 @@ main
         LDR     R0, =0x20000000
         STR     R3, [R0]
 
-stop
-        B       stop
->>>>>>> 5fc1055288f07277b7f44076bc16c77a7ea20ff2
+
 
         END
