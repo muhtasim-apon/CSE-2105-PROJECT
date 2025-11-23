806c9348f99513092fcf4bda5245b8083c24e852
Partho Kumar Mondal <partho2006ss@gmail.com>
2025-11-23 19:40:21 +0600

    Add files via upload
    ROR implementation

--- partho.s
+++ partho.s
@@ -3,6 +3,7 @@
 
 partho_main
         BL      Add_16bit_Function
+		BL		Rotation
         BX      LR
 
 Add_16bit_Function
@@ -11,10 +12,23 @@ Add_16bit_Function
 
         MOVW    R2, #0xFFFF
         MOVT    R2, #0x0000
-
+		
         ADD     R0, R1, R2       ; 1 + 65535 = 65536
         MOVT    R0, #0x0000
-
         BX      LR
+
+Rotation
+		LDR     R0, =0x1234      ; 16-bit value
+        MOV     R1, #5           ; rotate amount n = 4
+        UXTH    R0, R0           ; R0 = R0 & 0xFFFF
+        ; Compute (16 - n)
+        MOV     R2, #16
+        SUB     R3, R2, R1       ; R3 = 16 - n
+        LSR     R4, R0, R1
+        LSL     R5, R0, R3
+        ORR     R6, R4, R5
+        ; Mask to 16 bits
+        UXTH    R6, R6           ; R6 = final 16-bit rotate
+		BX		LR
 
         END
