688971d580f54602ab7f101490566d45cf4286c9
Faiaz-Iqbal <faiazibneiqbal@gmail.com>
2025-11-23 21:04:37 +0600

    Add files via upload

---
+++ apon.s
@@ -0,0 +1,20 @@
+        AREA apon, CODE, READONLY
+        EXPORT apon_main
+
+apon_main
+        ; Load a number into R1 (example: 0x5)
+        MOV    R1, #5
+
+        ; Load shift amount into R2 (example: shift left by 1)
+        MOV   R2, #1
+
+        ; Perform LSL: R3 = R1 << R2
+        LSL    R3, R1, R2
+
+        ; Store the result at memory address 0x20000000
+        LDR     R0, =0x20000000
+        STR     R3, [R0]
+
+
+
+        END
